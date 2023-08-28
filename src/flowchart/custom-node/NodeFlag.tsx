5e300b59471268dae397d98396744346db728c64
icepeng <typ0@naver.com>
2023-08-29 08:36:24 +0900

    feat: improve checkFlag render on node
    - Render the name of checkFlag when the node has only one flag.

--- src/flowchart/custom-node/NodeFlag.tsx
+++ src/flowchart/custom-node/NodeFlag.tsx
@@ -7,9 +7,18 @@ export interface NodeFlagProps {
 const NodeFlag: React.FC<NodeFlagProps> = ({ nodeData }) => {
   const checkFlags = nodeData.CheckFlags;
   const setFlags = nodeData.SetFlags;
+  const checkFlagCount = checkFlags.flatMap(
+    (flagGroup) => flagGroup.Flags
+  ).length;
+  const firstFlag = checkFlags[0]?.Flags[0].Name;
   return (
     <div>
-      {checkFlags.length > 0 && <span>(Flag Check)</span>}
+      {checkFlags.length > 0 &&
+        (checkFlagCount === 1 ? (
+          <span>({firstFlag})</span>
+        ) : (
+          <span>(Flag Check)</span>
+        ))}
       {setFlags.length > 0 && <span>(Flag Set)</span>}
     </div>
   );
